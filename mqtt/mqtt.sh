4f515ebcd1378078e89366161e5ba0f3c06f11d4
Henning Bjørgo <henning.bjorgo@outlook.com>
2019-10-11 23:46:30 +0200

    MQTT

--- mqtt/mqtt.sh
+++ mqtt/mqtt.sh
@@ -3,6 +3,13 @@
 ### Install ###
 apt-get install mosquitto mosquitto-clients
 
+### Configuration ###
+echo "allow_anonymous false" | tee -a /etc/mosquitto/conf.d/custom.conf
+echo "password_file /etc/mosquitto/users" | tee -a /etc/mosquitto/conf.d/custom.conf
+
+### Create username and password file ###
+mosquitto_passwd -c /etc/mosquitto/users mqttbroker
+
 ### Enable broker ###
 systemctl enable mosquitto
 
