9df132a6cdbc140a555aba8df651ab7deb17c31b
Henning Bjørgo <henning.bjorgo@outlook.com>
2019-10-11 20:03:42 +0200

    Init script

--- init/init.sh
+++ init/init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+### Hostname ###
+read -p "Do you want to set hostname? [y/N] " prompt
+if [[ $prompt == "y" || $prompt == "Y" ]]
+then
+    read -p "Please enter new hostname: " hostnamevar
+    hostname $hostnamevar
+    echo "Hostname set to $hostnamevar"
+fi
+
+### Update ###
 echo "Updating installed software..."
 apt-get update
 apt-get -y upgrade
 echo "Done"
-echo "Please enter new hostname:"
-read -p 'Hostname: ' hostnamevar
-echo "Hostname set to $hostnamevar"
