cdd06ca10abd141bf5c2c5022de65e77d92e40a1
Henning Bjørgo <henning.bjorgo@outlook.com>
2019-10-11 18:58:32 +0200

    Init script

--- init/init.sh
+++ init/init.sh
@@ -4,7 +4,6 @@ echo "Updating installed software..."
 apt-get update
 apt-get upgrade
 echo "Done"
-
 echo "Please enter new hostname:"
 read -p 'Hostname: ' hostnamevar
 echo "Hostname set to $hostnamevar"
