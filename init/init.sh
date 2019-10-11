f99c9135530769caaca9380111af4079a242149d
Henning Bjørgo <henning.bjorgo@outlook.com>
2019-10-11 21:01:45 +0200

    Init script

--- init/init.sh
+++ init/init.sh
@@ -4,14 +4,20 @@
 read -p "Do you want to set hostname? [y/n]? " prompt
 if [[ $prompt =~ [yY] ]]
 then
-    source ./sethostname.sh
+    read -p "Please enter new hostname: " hostnamevar
+echo $hostname >> /etc/hostname
+sed -i 's/raspberrypi/$hostname/g' /etc/hosts
+hostnamectl set-hostname "$hostname"
+systemctl restart avahi-daemon
+echo "Hostname set to $hostnamevar"
 else
     echo "Skipped setting hostname"
 fi
 
 ### Update ###
 echo "Updating installed software..."
-source ./update.sh
+apt-get update
+apt-get -y upgrade
 echo "Done"
 
 ### Reboot ###
