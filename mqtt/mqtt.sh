35d96f3135ec9e4496696fc31c5859d4d87c69a5
Henning Bjørgo <henning.bjorgo@outlook.com>
2019-10-12 00:01:36 +0200

    MQTT

--- mqtt/mqtt.sh
+++ mqtt/mqtt.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 ### Install ###
+echo "Installing Mosquitto..."
 apt-get install mosquitto mosquitto-clients
+echo "Done"
 
 ### Configuration ###
+echo "Writing configuration..."
 echo "allow_anonymous false" | tee -a /etc/mosquitto/conf.d/custom.conf
 echo "password_file /etc/mosquitto/users" | tee -a /etc/mosquitto/conf.d/custom.conf
+echo "Done"
 
 ### Create username and password file ###
+echo "Setting up username and password..."
 mosquitto_passwd -c /etc/mosquitto/users mqttbroker
+echo "Done"
 
-### Run onbackground as a daemon ###
+### Run on background as a daemon ###
+echo "Running Mosquitto as daemon..."
 mosquitto -d
+echo "Done"
